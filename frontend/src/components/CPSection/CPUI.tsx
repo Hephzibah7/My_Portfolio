7cd2814ef1ab998c3bea8fa8e197df3aab7e7c50
Hephzibah Ranjan <hephzibahranjan@gmail.com>
2026-07-04 01:08:30 +0530

    feat:Responsive CP  page

--- frontend/src/components/CPSection/CPUI.tsx
+++ frontend/src/components/CPSection/CPUI.tsx
@@ -1,13 +1,13 @@
 import { motion } from "framer-motion";
 function CPUI() {
     return (
-        <div className='w-full h-contain pl-20 pr-20 bg-gray-200'>
-            <div className='w-full pl-20 pr-20 '>
+        <div className='w-full h-contain lg:pl-20 lg:pr-20 bg-gray-200'>
+            <div className='w-full lg:pl-20 lg:pr-20 p-10'>
                 <motion.div
                     initial={{ opacity: 0, scale: 0.5 }}
                     whileInView={{ opacity: 1, scale: 1 }}
                     transition={{ duration: 0.8 }}
-                    className='p-20 ml-30 mr-20 pl-20  gap-20'>
+                    className='lg:p-20 lg:ml-30 lg:mr-20 lg:pl-20  gap-20'>
                     <div className='font-bold tracking-wide p-1 h-contain bg-gray-300 rounded uppercase w-fit'>why choose me?</div>
                     <h1 className='uppercase text-[50px] font-bold tracking-wide '>Coding Profile</h1>
                 </motion.div>
@@ -15,7 +15,7 @@ function CPUI() {
                     <div className='text-orange-600'>|</div>
                     <div className='font-bold  text-xs m-2 tracking-wide p-1 bg-gray-300 rounded uppercase w-fit'>Consistency Oriented</div>
                 </div>
-                <div className='grid grid-flow-col grid-rows-3 gap-7'>
+                <div className='flex flex-col  lg:w-fit gap-10 lg:grid lg:grid-flow-col lg:grid-rows-3 lg:gap-7'>
 
                     <motion.div
                         initial={{ opacity: 0, x: 100 }}
@@ -40,7 +40,7 @@ function CPUI() {
                     <div className='text-orange-600'>|</div>
                     <div className='font-bold  text-xs m-2 tracking-wide p-1 bg-gray-300 rounded uppercase w-fit'>2+ Years of consistent hardwork</div>
                 </div>
-                <div className="grid grid-flow-col grid-rows-2 gap-4 w-full mt-10">
+                <div className="flex flex-col gap-10 lg:grid lg:grid-flow-col lg:grid-rows-2 lg:gap-4 w-full mt-10">
                     <motion.div
                         initial={{ opacity: 0, x: 100 }}
                         whileInView={{ opacity: 1, x: 0 }}
